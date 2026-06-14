0dcb32404477ab39a08cc03867609cfd17099e78
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-06-14 20:28:40 -0300

    feat(company): update company entity

--- fleet/fleet/src/main/java/com/bortolanza/fleet/modules/company/mapper/CompanyMapper.java
+++ fleet/fleet/src/main/java/com/bortolanza/fleet/modules/company/mapper/CompanyMapper.java
@@ -4,6 +4,7 @@ import com.bortolanza.fleet.modules.company.dto.in.CompanyRequestDTO;
 import com.bortolanza.fleet.modules.company.dto.out.CompanyResponseDTO;
 import com.bortolanza.fleet.modules.company.entity.Company;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CompanyMapper {
@@ -11,4 +12,6 @@ public interface CompanyMapper {
     Company toEntity(CompanyRequestDTO dto);
 
     CompanyResponseDTO toResponseDTO(Company entity);
+
+    void updateEntity(CompanyRequestDTO dto, @MappingTarget Company entity);
 }
